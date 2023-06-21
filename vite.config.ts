3b4b42d6885885460635bb3c677578d90ce3f25e
Nico Del Rosario <57257533+Nirioppai@users.noreply.github.com>
2023-06-21 12:16:09 +0800

    Edited Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+// eslint-disable-next-line import/default
+import checker from 'vite-plugin-checker';
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    outDir: './dist',
+  },
+  publicDir: './public',
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    react({
+      include: '**/*.{jsx,tsx}',
+    }),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
   base: '/dynamic-pos-deployable/',
+  server: {
+    port: 3000,
+  },
 });
